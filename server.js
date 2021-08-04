d8c727ff05fe724d3f12534bac360b977f849484
skosana721 <wonganiskosana@gmail.com>
2021-08-04 11:43:31 +0200

    exporting the starter freecodecamp starter pack

--- server.js
+++ server.js
@@ -1,53 +1,25 @@
-'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const bcrypt = require("bcrypt");
+const app = express();
 fccTesting(app);
 const saltRounds = 12;
-const myPlaintextPassword = 'sUperpassw0rd!';
-const someOtherPlaintextPassword = 'pass123';
-
+const myPlaintextPassword = "sUperpassw0rd!";
+const someOtherPlaintextPassword = "pass123";
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
-//END_ASYNC
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log("hashed", hash);
+  //END_ASYNC
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log("compare", res);
+  });
+});
 
 //START_SYNC
 
-
-
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(process.env.PORT || 3000, () => {});
